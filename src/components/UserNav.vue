c3c2a3290e5d6c7a41f6848c4e285a0c8c8d5ae6
Mehdi <hiiiken@live.com>
2022-07-31 18:23:30 +0100

    Adding logout function

---
+++ src/components/UserNav.vue
@@ -0,0 +1,32 @@
+<template>
+  <div
+    class="user-profile bg-p inline-block w-12 h-12 rounded-full text-center pt-3 text-heading"
+  >
+    <p>H</p>
+  </div>
+  <ul>
+    <li><a @click="Logout">Logout</a></li>
+  </ul>
+</template>
+
+<script lang="ts">
+import router from "@/router";
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "UserNav",
+
+  setup() {
+    const Logout = () => {
+      localStorage.removeItem("my-token");
+      router.push("/");
+    };
+
+    return {
+      Logout,
+    };
+  },
+});
+</script>
+
+<style scoped></style>
--- src/views/DashboardHome.vue
+++ src/views/DashboardHome.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>You reached the dashboard! Welcome!</h1>
+  <user-nav />
 </template>
 
 <script lang="ts">
 // import axios from 'axios';
 import { defineComponent } from "@vue/runtime-core";
 import { useRouter } from "vue-router";
+import UserNav from "@/components/UserNav.vue";
 
 export default defineComponent({
   name: "DashboardHome",
+  components: {
+    UserNav,
+  },
   setup() {
     const router = useRouter();
 
