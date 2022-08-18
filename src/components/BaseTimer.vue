cb9fdc114e7ced9d43188fd092a8ff8cb9cb6fe5
Mehdi <hiiiken@live.com>
2022-08-18 13:30:15 +0100

    Implementing a countdown timer

---
+++ src/components/BaseTimer.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="base-timer">
+    <svg
+      class="base-timer__svg"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g class="base-timer__circle">
+        <circle class="base-timer__path-elapsed" cx="50" cy="50" r="46.5" />
+      </g>
+    </svg>
+    <span class="base-timer__label">
+      {{ formattedTimeLeft }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "BaseTimer",
+  props: {
+    timeLeft: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped lang="scss">
+/* Sets the containers height and width */
+.base-timer {
+  position: relative;
+  width: 300px;
+  height: 300px;
+  /* Removes SVG styling that would hide the time label */
+  &__circle {
+    fill: none;
+    stroke: none;
+  }
+  /* The SVG path that displays the timer's progress */
+  &__path-elapsed {
+    stroke-width: 7px;
+    stroke: grey;
+  }
+
+  &__label {
+    position: absolute;
+
+    /* Size should match the parent container */
+    width: 300px;
+    height: 300px;
+    /* Keep the label aligned to the top */
+    top: 0;
+    /* Create a flexible box that centers content vertically and horizontally */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    /* Sort of an arbitrary number; adjust to your liking */
+    font-size: 48px;
+  }
+}
+</style>
--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -9,50 +9,142 @@
   <button @click="timerReset" v-if="timerRunning" class="bg-p p-4 m-4">
     Reset
   </button>
-  <!-- <p>{{ formattedElapsedTime }}</p> -->
   <p>{{ message }}</p>
-  <p>{{ formattedElapsedTime }}</p>
+
+  <div class="base-timer">
+    <svg
+      class="base-timer__svg"
+      viewBox="0 0 100 100"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g class="base-timer__circle">
+        <circle
+          class="base-timer__path-elapsed"
+          cx="50"
+          cy="50"
+          r="45"
+        ></circle>
+        <path
+          :stroke-dasharray="circleDasharray"
+          class="base-timer__path-remaining"
+          :class="remainingPathColor"
+          d="
+            M 50, 50
+            m -45, 0
+            a 45,45 0 1,0 90,0
+            a 45,45 0 1,0 -90,0
+          "
+        ></path>
+      </g>
+    </svg>
+    <span class="base-timer__label">{{ formattedTimeLeft }}</span>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import { defineComponent } from "vue";
-import { ref, computed } from "vue";
+import { ref, computed, watch } from "vue";
+
+const FULL_DASH_ARRAY = 283;
+const WARNING_THRESHOLD = 10;
+const ALERT_THRESHOLD = 5;
+
+const COLOR_CODES = {
+  info: {
+    color: "green",
+  },
+  warning: {
+    color: "orange",
+    threshold: WARNING_THRESHOLD,
+  },
+  alert: {
+    color: "red",
+    threshold: ALERT_THRESHOLD,
+  },
+};
+
+const TIME_LIMIT = 20;
 
 export default defineComponent({
   name: "StopWatch",
-
   setup() {
     let message = ref("Let the countdown begin!!");
     let timerRunning = ref(false);
-    let timer = ref(undefined);
-    let elapsedTime = ref(0);
 
-    const formattedElapsedTime = computed(() => {
-      const date = new Date(null);
-      date.setSeconds(elapsedTime.value / 1000);
-      const utc = date.toUTCString();
-      return utc.substr(utc.indexOf(":") + 1, 5);
+    let timePassed = ref(0);
+    let timerInterval = ref(null);
+
+    let circleDasharray = computed(() => {
+      return `${(timeFraction.value * FULL_DASH_ARRAY).toFixed(0)} 283`;
     });
 
+    let timeLeft = computed(() => {
+      return TIME_LIMIT - timePassed.value;
+    });
+
+    const formattedTimeLeft = computed(() => {
+      const timeLeftt = timeLeft.value;
+      const minutes = Math.floor(timeLeftt / 60);
+      let seconds = timeLeftt % 60;
+
+      if (seconds < 10) {
+        seconds = `0${seconds}`;
+      }
+
+      return `${minutes}:${seconds}`;
+    });
+
+    let timeFraction = computed(() => {
+      const rawTimeFraction = timeLeft.value / TIME_LIMIT;
+      return rawTimeFraction - (1 / TIME_LIMIT) * (1 - rawTimeFraction);
+    });
+
+    let remainingPathColor = computed(() => {
+      const { alert, warning, info } = COLOR_CODES;
+
+      if (timeLeft.value <= alert.threshold) {
+        return alert.color;
+      } else if (timeLeft.value <= warning.threshold) {
+        return warning.color;
+      } else {
+        return info.color;
+      }
+    });
+
+    watch(timeLeft, (newValue) => {
+      if (newValue === 0) {
+        onTimesUp();
+      }
+    });
+
+    // onMounted(() => {
+    //   startTimer();
+    // });
+
+    let onTimesUp = () => {
+      clearInterval(timerInterval.value);
+    };
+
+    let startTimer = () => {
+      timerInterval.value = setInterval(() => (timePassed.value += 1), 1000);
+    };
+
+    // Mine
     const timerStart = () => {
       message.value = `It's happening!`;
       timerRunning.value = true;
-      timer.value = setInterval(() => {
-        elapsedTime.value += 1000;
-      }, 1000);
+      startTimer();
     };
 
     const timerPause = () => {
       message.value = "Pause for now";
       timerRunning.value = false;
-      clearInterval(timer.value);
+      onTimesUp();
     };
 
     const timerReset = () => {
       message.value = "Start again?";
       timerRunning.value = false;
-      elapsedTime.value = 0;
-      clearInterval(timer.value);
     };
 
     return {
@@ -61,40 +153,71 @@ export default defineComponent({
       timerStart,
       timerPause,
       timerReset,
-      timer,
-      elapsedTime,
-      formattedElapsedTime,
+      formattedTimeLeft,
+      timePassed,
+      timerInterval,
+      circleDasharray,
+      timeLeft,
+      timeFraction,
+      remainingPathColor,
+      onTimesUp,
+      startTimer,
     };
-
-    // let elapsedTime = ref(0);
-    // let timer = ref(undefined);
-    // const formattedElapsedTime = computed(() => {
-    //   const date = new Date(null);
-    //   date.setSeconds(elapsedTime.value / 1000);
-    //   const utc = date.toUTCString();
-    //   return utc.substr(utc.indexOf(":") - 2, 8);
-    // });
-    // const start = () => {
-    //   timer.value = setInterval(() => {
-    //     elapsedTime.value += 1000;
-    //   }, 1000);
-    // };
-    // const stop = () => {
-    //   console.log("stop");
-    //   clearInterval(timer.value);
-    // };
-    // const reset = () => {
-    //   elapsedTime.value = 0;
-    //   clearInterval(timer.value);
-    // };
-    // return {
-    //   elapsedTime,
-    //   timer,
-    //   formattedElapsedTime,
-    //   start,
-    //   stop,
-    //   reset,
-    // };
   },
 });
 </script>
+
+<style scoped lang="scss">
+.base-timer {
+  position: relative;
+  width: 300px;
+  height: 300px;
+
+  &__svg {
+    transform: scaleX(-1);
+  }
+
+  &__circle {
+    fill: none;
+    stroke: none;
+  }
+
+  &__path-elapsed {
+    stroke-width: 7px;
+    stroke: grey;
+  }
+
+  &__path-remaining {
+    stroke-width: 7px;
+    stroke-linecap: round;
+    transform: rotate(90deg);
+    transform-origin: center;
+    transition: 1s linear all;
+    fill-rule: nonzero;
+    stroke: currentColor;
+
+    &.green {
+      color: rgb(65, 184, 131);
+    }
+
+    &.orange {
+      color: orange;
+    }
+
+    &.red {
+      color: red;
+    }
+  }
+
+  &__label {
+    position: absolute;
+    width: 300px;
+    height: 300px;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 48px;
+  }
+}
+</style>
